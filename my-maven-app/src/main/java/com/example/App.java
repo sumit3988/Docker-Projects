870a5b3fcea938ca7e8227c6cb4a43517be10209
Sumit <sumitk9133@gmail.com>
2026-09-24 14:17:44 +0530

    Added 2 new entries

---
+++ my-maven-app/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Automated Maven Build Successful!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
